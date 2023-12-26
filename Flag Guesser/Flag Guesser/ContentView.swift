52759c82884203c866bf54da3365efd1d27d5427
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-12-25 18:45:39 -0800

    Added main functionality + UI tweaks

--- Flag Guesser/Flag Guesser/ContentView.swift
+++ Flag Guesser/Flag Guesser/ContentView.swift
@@ -6,20 +6,63 @@
 //
 
 import SwiftUI
-
+ 
 struct ContentView: View {
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
+    
     var body: some View {
-//        LinearGradient(stops: [
-//            .init(color:.mint, location: 0),
-//            .init(color:.blue, location: 0.8)
-//        ], startPoint: .top, endPoint: .bottom)
-//        .ignoresSafeArea()
-//        RadialGradient(colors: [.blue, .mint], center: .center, startRadius: 0, endRadius: 250)
-//        AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
-        Text("Your content")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .foregroundStyle(.white)
-            .background(.blue.gradient)
+        ZStack {
+            LinearGradient(colors: [.mint, .black], startPoint: .top, endPoint: .bottom)                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy))
+                    
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.semibold))
+                }
+                
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .clipShape(.capsule)
+                            .shadow(radius: 5)
+                    }
+                }
+            }
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is: \(score)")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true;
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
