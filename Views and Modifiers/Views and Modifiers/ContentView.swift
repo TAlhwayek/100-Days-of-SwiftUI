3db772309065f146f59f1274b95bb3f75c0c270e
TAlhwayek <alhwat2@unlv.nevada.edu>
2023-12-28 03:18:58 -0800

    Test project - No importance

--- Views and Modifiers/Views and Modifiers/ContentView.swift
+++ Views and Modifiers/Views and Modifiers/ContentView.swift
@@ -7,15 +7,88 @@
 
 import SwiftUI
 
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .background(.blue)
+            .clipShape(.capsule)
+    }
+}
+
+struct CapsuleTextMod: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .background(.blue)
+            .clipShape(.capsule)
+    }
+}
+
+
+
+struct Title : ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.red)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+    
+    func waterMarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        CapsuleText(text: "CHEESE")
+            .foregroundStyle(.red)
+        CapsuleText(text: "I like it")
+            .foregroundStyle(.indigo)
+        Text("HELLO")
+        // Before extension
+        //  .modifier(Title())
+        // After extension
+            .titleStyle()
+        
+        Color.blue
+            .frame(width: 300, height: 200)
+            .waterMarked(with: "Tony Alhwayek")
+        
+        Text("HELLO 2.0")
+            .modifier(CapsuleTextMod(text: "FISH"))
+
     }
 }
 
