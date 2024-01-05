50c24731bdd20d4e56f922a2d2a1942ff721f400
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-01-05 04:40:41 -0800

    Most of the project is done

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Tony Alhwayek on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
