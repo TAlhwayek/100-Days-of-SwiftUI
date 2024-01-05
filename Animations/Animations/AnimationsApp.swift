9bcce9747ba6a90cbdf72227e033aa4b7356d9f0
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-01-05 05:14:26 -0800

    Project created

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Tony Alhwayek on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
