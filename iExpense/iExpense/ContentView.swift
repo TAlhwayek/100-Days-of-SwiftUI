a0eb86c62971e521337417db05bc4739f7344b62
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-01-06 06:21:22 -0800

    Multiple different tutorials

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -5,17 +5,76 @@
 //  Created by Tony Alhwayek on 1/6/24.
 //
 
+import Observation
 import SwiftUI
 
-struct ContentView: View {
+@Observable
+class User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+    let name: String
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Dismiss") {
+            dismiss()
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @State private var user = User()
+    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("Your name is \(user.firstName) \(user.lastName)")
+                
+                TextField("First name", text: $user.firstName)
+                TextField("Last name", text: $user.lastName)
+                
+                Button("Show Sheet") {
+                    showingSheet.toggle()
+                }
+                .sheet(isPresented: $showingSheet) {
+                    SecondView(name: "Tony")
+                }
+                
+                
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Tap count: \(tapCount)") {
+                    tapCount += 1
+                    
+                    UserDefaults.standard.set(tapCount, forKey: "Tap")
+                }
+                
+                Button("Add numbers") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
+        }
+        
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
