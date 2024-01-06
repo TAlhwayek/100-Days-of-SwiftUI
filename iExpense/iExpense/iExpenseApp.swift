96f641b8cd8e1b3427bc82aea03e0c8bd33a0704
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-01-06 05:22:28 -0800

    Project created

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Tony Alhwayek on 1/6/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
