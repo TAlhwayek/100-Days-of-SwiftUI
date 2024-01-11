dfc34f1d446d1f673b932e6e89c385ca2660c6c1
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-01-11 09:14:35 -0800

    Project created

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Tony Alhwayek on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
