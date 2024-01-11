e9b88c31bec5b0f6414dca9ca7a43d36716df891
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-01-10 17:03:49 -0800

    Challenge started

---
+++ Challenge #3 - Multipliplaytion/Challenge #3 - Multipliplaytion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge #3 - Multipliplaytion
+//
+//  Created by Tony Alhwayek on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
