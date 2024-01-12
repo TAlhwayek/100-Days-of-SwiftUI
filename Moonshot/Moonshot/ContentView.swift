bd056697e3772b5d47c60cb2d541d4f3c7764c28
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-01-12 11:07:16 -0800

    Trying to recreate sliding date picker. Failed.

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,15 +7,56 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CustomText: View {
+    let text: String
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("Creating a new custom text")
+        self.text = text
+    }
+}
+
+struct ContentView: View {
+    @State private var monthPicker = 0
+    
+    let months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+    
+    var body: some View {
+        ScrollView (.horizontal){
+            LazyHStack(spacing: 8) {
+                ForEach(1..<366) { day in
+                    Button {
+                        print("TODAY")
+                    } label: {
+                        Text("\(months[monthPicker])\n \(day)")
+                    }
+                    .font(.system(size: 12))
+                    .foregroundStyle(.black)
+                    .frame(width: 65, height: 80)
+                    .overlay (
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.purple, lineWidth: 2)
+                    )
+                    .onAppear {
+                        checkMonth(day: day)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .padding(2)
         }
-        .padding()
+    }
+    
+    func checkMonth(day: Int) {
+        if day <= 31 {
+            monthPicker = 0
+        } else if day <= 59 {
+            monthPicker = 1
+        }
     }
 }
 
