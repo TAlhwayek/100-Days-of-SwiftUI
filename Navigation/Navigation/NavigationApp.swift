f923e8d1d048ded1c7890f00b6322a8a0942831d
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-01-18 22:20:51 -0800

    Project created

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Tony Alhwayek on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
