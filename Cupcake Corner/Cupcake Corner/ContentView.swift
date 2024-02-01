fb73e5ff0bf01ae44248b638f80d83b8d779332c
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-01-31 22:47:16 -0800

    Using AsyncImage

--- Cupcake Corner/Cupcake Corner/ContentView.swift
+++ Cupcake Corner/Cupcake Corner/ContentView.swift
@@ -11,55 +11,23 @@ enum HttpErrors: Error {
     case badURL, badResponse, invalidData
 }
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
-    
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                
-                Text(item.collectionName)
-                
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("Error loading image.")
+            } else {
+                ProgressView()
             }
         }
-        .task {
-            do {
-                try await loadData()
-            } catch {
-                print("ERROR")
-            }
-        }
+        .frame(width: 200, height: 200)
     }
+}
     
-    func loadData() async throws {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=dream+theater&entity=song") else {
-            throw HttpErrors.badURL
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            throw HttpErrors.invalidData
-        }
+    #Preview {
+        ContentView()
     }
-}
-
-#Preview {
-    ContentView()
-}
