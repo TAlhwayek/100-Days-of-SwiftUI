18b8686baa7e58267aa8791dfb78cd9faa011dea
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-02-01 18:12:45 -0800

    Added sections about ordering

---
+++ Cupcake Corner/Order.swift
@@ -0,0 +1,27 @@
+//
+//  Order.swift
+//  Cupcake Corner
+//
+//  Created by Tony Alhwayek on 2/1/24.
+//
+
+import Foundation
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    var specialRequestEnabled = false 
+    {
+        didSet {
+            if !specialRequestEnabled {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFrosting = false
+    var addSprinkles = false
+}
