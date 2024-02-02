fc2c3025de35b7a95fe02abc7349ae0c778f4373
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-02-01 17:57:21 -0800

    Learning about CoreHaptics

--- Cupcake Corner/Cupcake Corner/ContentView.swift
+++ Cupcake Corner/Cupcake Corner/ContentView.swift
@@ -5,18 +5,51 @@
 //  Created by Tony Alhwayek on 1/31/24.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
     @State private var counter = 0
+    @State private var engine: CHHapticEngine?
+    
     var body: some View {
-        Button("Tap count: \(counter)") {
-            counter += 1
+        Button("Play Haptic", action: complexSuccess)
+            .onAppear(perform: prepareHaptics)
+    }
+    
+    func prepareHaptics() {
+        // Check if device has haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
         }
-        .sensoryFeedback(.impact(weight: .heavy, intensity: 1), trigger: counter)
+    }
+    
+    func complexSuccess() {
+        // Check if device has haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription)")
+        }
     }
 }
-    
-    #Preview {
-        ContentView()
-    }
+
+#Preview {
+    ContentView()
+}
