87f3f384d9a5a3da8478741fa08ad965530701da
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-02-02 22:14:59 -0800

    Project created

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Tony Alhwayek on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
