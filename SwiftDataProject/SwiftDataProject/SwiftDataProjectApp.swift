b34718721e9f4d13d5f64f2c46c0607cd730af3c
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-02-06 22:05:33 -0800

    Project created

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Tony Alhwayek on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
