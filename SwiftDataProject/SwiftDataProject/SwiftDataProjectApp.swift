a5f63b346a0e47fbd82d82ed2fb6562d7ef17f5b
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-02-08 20:23:52 -0800

    Using swiftdata with Bindable

--- SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Tony Alhwayek on 2/6/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
