5662fb1d9eb37087f2a76a8464c5e4b36f0e9fd9
TAlhwayek <alhwat2@unlv.nevada.edu>
2024-02-10 15:38:05 -0800

    Separated toolbaritems

--- Expense App/Expense App/ContentView.swift
+++ Expense App/Expense App/ContentView.swift
@@ -29,32 +29,38 @@ struct ContentView: View {
             }
             .navigationTitle("iExpense")
             .toolbar {
-                NavigationLink(destination: AddView()) {
-                    Image(systemName: "plus")
-                }
-                
-                Menu("Filter", systemImage: "line.3.horizontal.decrease.circle") {
-                    Picker("Filter", selection: $filterSelection) {
-                        Text("All Expenses")
-                            .tag("All")
-                        Text("Personal Only")
-                            .tag("Personal")
-                        Text("Business Only")
-                            .tag("Business")
+                // Right side
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    NavigationLink(destination: AddView()) {
+                        Image(systemName: "plus")
                     }
                 }
-                
-                Menu("Sort", systemImage: "arrow.up.arrow.down") {
-                    Picker("Sort", selection: $sortOrder) {
-                        Text("Sort by Name")
-                            .tag([
-                                SortDescriptor(\ExpenseItem.name)
-                            ])
-                        
-                        Text("Sort by Amount")
-                            .tag([
-                                SortDescriptor(\ExpenseItem.amount)
-                            ])
+
+                // Left side
+                ToolbarItemGroup(placement: .topBarLeading) {
+                    Menu("Filter", systemImage: "line.3.horizontal.decrease.circle") {
+                        Picker("Filter", selection: $filterSelection) {
+                            Text("All Expenses")
+                                .tag("All")
+                            Text("Personal Only")
+                                .tag("Personal")
+                            Text("Business Only")
+                                .tag("Business")
+                        }
+                    }
+                    
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                        Picker("Sort", selection: $sortOrder) {
+                            Text("Sort by Name")
+                                .tag([
+                                    SortDescriptor(\ExpenseItem.name)
+                                ])
+                            
+                            Text("Sort by Amount")
+                                .tag([
+                                    SortDescriptor(\ExpenseItem.amount)
+                                ])
+                        }
                     }
                 }
             }
